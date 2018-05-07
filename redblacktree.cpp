971bb9f4cae799e80c010a3fad1d4561d6a5d51c
Andrii Perun <andrewfreelan@gmail.com>
2018-05-07 19:39:40 +0300

    Red-black tree module added

---
+++ redblacktree.cpp
@@ -0,0 +1,351 @@
+#include "redblacktree.h"
+
+RedBlackTree::RedBlackTree() {
+    root = nullptr;
+}
+
+void RedBlackTree::addNode(Node *node) {
+    if (root == nullptr) {
+        node->setcolor(BLACK);
+        root = node;
+        return;
+    } else {
+        Node* curNode = root;
+        while (!curNode->isNill()) {
+            if (curNode->data > node->data) {
+                if (curNode->left->isNill()) {
+                    delete curNode->left;
+                    curNode->left = node;
+                    node->parent = curNode;
+                    break;
+                } else {
+                    curNode = curNode->left;
+                }
+            } else if (curNode->data < node->data) {
+                if (curNode->right->isNill()) {
+                    delete curNode->right;
+                    curNode->right = node;
+                    node->parent = curNode;
+                    break;
+                } else {
+                    curNode = curNode->right;
+                }
+            } else {
+                return;
+            }
+        }
+    }
+    Node* tmpNode = node;
+    Node* uncle = nullptr;
+    while (!tmpNode->isRoot() && tmpNode->getColor() == RED &&
+           tmpNode->parent->getColor() == RED) {
+        if (tmpNode->isRoot()) {
+            tmpNode->setcolor(BLACK);
+            break;
+        }
+
+        if (tmpNode->parent->isLeftChild()) {
+            uncle = tmpNode->parent->parent->right;
+        } else {
+            uncle = tmpNode->parent->parent->left;
+        }
+
+        // CASE A: Uncle is RED
+        if (uncle->getColor() == RED) {
+            tmpNode->parent->setcolor(BLACK);
+            uncle->setcolor(BLACK);
+            tmpNode->parent->parent->setcolor(RED);
+            tmpNode = tmpNode->parent->parent;
+        }
+        // CASE B: Uncle is BLACK
+        else {
+            if (tmpNode->isLeftChild() && tmpNode->parent->isLeftChild()) {
+                tmpNode->parent->setcolor(BLACK);
+                tmpNode->parent->parent->setcolor(RED);
+                tmpNode->parent->parent->rotateRight();
+            } else if (tmpNode->isRightChild() && tmpNode->parent->isLeftChild()) {
+                tmpNode->setcolor(BLACK);
+                tmpNode->parent->parent->setcolor(RED);
+                tmpNode->parent->rotateLeft();
+                tmpNode->parent->rotateRight();
+            } else if (tmpNode->isRightChild() && tmpNode->parent->isRightChild()) {
+                tmpNode->parent->setcolor(BLACK);
+                tmpNode->parent->parent->setcolor(RED);
+                tmpNode->parent->parent->rotateLeft();
+            } else if (tmpNode->isLeftChild() && tmpNode->parent->isRightChild()) {
+                tmpNode->setcolor(BLACK);
+                tmpNode->parent->parent->setcolor(RED);
+                tmpNode->parent->rotateRight();
+                tmpNode->parent->rotateLeft();
+            }
+        }
+    }
+    if (tmpNode->isRoot()) {
+        tmpNode->setcolor(BLACK);
+        root = tmpNode;
+    }
+}
+
+void RedBlackTree::deleteNode(Node *node) {
+    if (node == nullptr) {
+        return ;
+    }
+    Node* curNode = root;
+    while (!curNode->isNill()) {
+        if (curNode->data > node->data) {
+            curNode = curNode->left;
+        } else if (curNode->data < node->data) {
+            curNode = curNode->right;
+        } else {
+            Node* deletedNode = inorderSuccessor(curNode);
+            curNode->data = deletedNode->data;
+            fixDeletion(deletedNode);
+            break;
+        }
+    }
+}
+
+void RedBlackTree::addNode(const Data &data) {
+    Node* node = new Node(data);
+
+    node->left = new Node(node);
+    node->right = new Node(node);
+
+    node->left->nill = node->right->nill = true;
+
+    node->left->setcolor(BLACK);
+    node->right->setcolor(BLACK);
+
+    addNode(node);
+}
+
+void RedBlackTree::deleteNode(const Data &data) {
+    Node* node = new Node(data);
+    deleteNode(node);
+}
+
+void RedBlackTree::printTree(Node* curNode) {
+    if (curNode->isNill()) {
+        return;
+    }
+    printTree(curNode->left);
+    std::cout << curNode->data.key << ' ';
+    printTree(curNode->right);
+}
+
+void RedBlackTree::printTree() {
+
+    printTree(root);
+}
+
+Node *RedBlackTree::inorderSuccessor(Node *node) {
+    if (node->right->isNill()) {
+        return node;
+    }
+
+    Node* curNode = node->right;
+    while(!curNode->left->isNill()) {
+        curNode = curNode->left;
+    }
+    return curNode;
+}
+
+void RedBlackTree::fixDeletion(Node *deletedNode) {
+    Node* u;
+    if (deletedNode->right->isNill()) {
+        u = deletedNode->left;
+        delete deletedNode->right;
+    } else {
+        u = deletedNode->right;
+        delete deletedNode->left;
+    }
+    u->parent = deletedNode->parent;
+    if (deletedNode->parent && deletedNode->isLeftChild()) {
+        deletedNode->parent->left = u;
+    } else if (deletedNode->parent && deletedNode->isRightChild()) {
+        deletedNode->parent->right = u;
+    }
+    // Case 2: if either u or v is Red
+    if (deletedNode->getColor() == RED || u->getColor() == RED) {
+        u->setcolor(BLACK);
+    }
+    // Case 3: If Both u and v are Black
+    else {
+        u->setcolor(DOUBLE_BLACK);
+        while (u->getColor() == DOUBLE_BLACK && !u->isRoot()) {
+            Node* s = u->parent->right;
+            if (u->isRightChild())
+                s = u->parent->left;
+            // Case A: If sibling s is black and at least one of sibling’s children is red
+            if (s->getColor() == BLACK &&
+                    (s->left->getColor() == RED || s->right->getColor() == RED) ) {
+                Node* r;
+                if (s->left->getColor() == RED) {
+                    r = s->left;
+                } else if (s->right->getColor() == RED) {
+                    r = s->right;
+                }
+                r->setcolor(BLACK);
+                u->setcolor(BLACK);
+                if (s->isLeftChild() && r->isLeftChild()) {
+                    s->parent->rotateRight();
+                } else if (s->isLeftChild() && r->isRightChild()) {
+                    s->rotateLeft();
+                    r->parent->rotateRight();
+                } else if (s->isRightChild() && r->isRightChild()) {
+                    s->parent->rotateLeft();
+                } else if (s->isRightChild() && r->isLeftChild()) {
+                    s->rotateRight();
+                    r->parent->rotateLeft();
+                }
+            }
+            // Case B: If sibling is black and its both children are black
+            else if (s->getColor() == BLACK && !(s->left->getColor() == RED || s->right->getColor() == RED)) {
+                s->setcolor(RED);
+                u->setcolor(BLACK);
+                if (u->parent->getColor() == BLACK) {
+                    u->parent->setcolor(DOUBLE_BLACK);
+                } else {
+                    u->parent->setcolor(BLACK);
+                }
+                u = u->parent;
+            }
+            // Case C: If sibling is red
+            else if (s->getColor() == RED) {
+                s->setcolor(BLACK);
+                s->parent->setcolor(RED);
+                if (s->isLeftChild()) {
+                    s->parent->rotateRight();
+                } else if (s->isRightChild()) {
+                    s->parent->rotateLeft();
+                }
+            }
+
+        }
+        if (u->isRoot()) {
+            u->setcolor(BLACK);
+            root = u;
+        }
+    }
+    delete deletedNode;
+}
+
+Data& Data::operator=(const Data &curNode) {
+    this->key = curNode.key;
+    return *this;
+}
+
+bool Data::operator <(const Data &data) {
+    return this->key < data.key;
+}
+
+bool Data::operator <=(const Data &data) {
+    return this->key <= data.key;
+}
+
+
+bool Data::operator >(const Data &data) {
+    return this->key > data.key;
+}
+
+Node::Node() {
+    left = right = parent = nullptr;
+    color = RED;
+    nill = false;
+}
+
+Node::Node(Node *nparent) {
+    left = right = nullptr;
+    this->parent = nparent;
+    color = RED;
+    nill = false;
+}
+
+Node::Node(const Data &data, Node* nparent) {
+    left = right = nullptr;
+    parent = nparent;
+
+    this->data = data;
+    color = RED;
+    nill = false;
+}
+
+void Node::setcolor(const Color & ncolor) {
+    this->color = ncolor;
+}
+
+Color Node::getColor() {
+    return color;
+}
+
+bool Node::isNill() {
+    return nill;
+}
+
+bool Node::isRoot() {
+    return parent == nullptr;
+}
+
+bool Node::isLeftChild() {
+    return parent->left == this;
+}
+
+bool Node::isRightChild() {
+    return parent->right == this;
+}
+
+void Node::swapData(Node & curNode) {
+    Node tmpNode = this->data;
+    this->data = curNode.data;
+    curNode.data = tmpNode.data;
+}
+
+void Node::rotateLeft() {
+    if (right->isNill()) {
+        return;
+    } else {
+        Node* tmpRight = right;
+
+        // connect right-left with this
+        right->left->parent = this;
+        right = right->left;
+
+        // connect parent with right
+        tmpRight->parent = parent;
+        if (isLeftChild() ) {
+            parent->left = tmpRight;
+        } else if (isRightChild() ) {
+            parent->right = tmpRight;
+        }
+
+        // reconnect this with right
+        tmpRight->left = this;
+        parent = tmpRight;
+    }
+}
+
+void Node::rotateRight() {
+    if (left->isNill()) {
+        return;
+    } else {
+        Node* tmpLeft = left;
+
+        // connect right-left with this
+        left->right->parent = this;
+        left = left->right;
+
+        // connect parent with right
+        tmpLeft->parent = parent;
+        if (parent && isRightChild() ) {
+            parent->right = tmpLeft;
+        } else if (parent && isLeftChild() ) {
+            parent->left = tmpLeft;
+        }
+
+        // reconnect this with right
+        tmpLeft->right = this;
+        parent = tmpLeft;
+    }
+}
+
+
---
+++ redblacktree.h
@@ -0,0 +1,76 @@
+#ifndef REDBLACKTREE_H
+#define REDBLACKTREE_H
+
+#include <iostream>
+
+
+enum Color {
+    RED, BLACK, DOUBLE_BLACK
+};
+
+struct Data {
+    int key;
+    Data() : key(0){}
+    Data(int key) : key(key) {}
+
+    Data& operator =(const Data&);
+    bool operator >(const Data&);
+    bool operator <(const Data&);
+    bool operator <=(const Data&);
+
+    friend std::ostream& operator<<(std::ostream& os, const Data& data) {
+        os << data.key;
+        return os;
+    }
+};
+
+
+struct Node {
+    Data data;
+    Color color;
+    bool nill;
+
+    Node* left;
+    Node* right;
+    Node* parent;
+
+    Node();
+    Node(Node* parent = nullptr);
+    Node(const Data& data, Node* nparent = nullptr);
+
+    void setcolor(const Color&);
+    Color getColor();
+
+    bool isNill();
+    bool isRoot();
+    bool isLeftChild();
+    bool isRightChild();
+
+    void swapData(Node&);
+
+    void rotateLeft();
+    void rotateRight();
+};
+
+class RedBlackTree {
+
+    Node* root;
+
+public:
+    RedBlackTree();
+
+    void addNode(Node*);
+    void deleteNode(Node*);
+
+    void addNode(const Data&);
+    void deleteNode(const Data&);
+
+    void printTree(Node* curNode);
+    void printTree();
+
+    Node* inorderSuccessor(Node* node);
+    void fixDeletion(Node* deletedNode);
+
+};
+
+#endif // REDBLACKTREE_H
